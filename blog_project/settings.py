abf01b9d6bee500a286e7ef4f119b3f844550ad9
Babu Thomas <babuthomas138@gmail.com>
2018-02-16 17:25:39 +0530

    Add login feature

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -124,3 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGIN_REDIRECT_URL = 'home'
