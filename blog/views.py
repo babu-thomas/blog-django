60a75f50545977d7970bb00f27190629ff763202
Babu Thomas <babuthomas138@gmail.com>
2018-02-16 06:36:52 +0530

    Make homepage work

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
