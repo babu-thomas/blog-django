1bcbebbfb5a6015a2eec6fd47a78876d0167cd1f
Babu Thomas <babuthomas138@gmail.com>
2018-02-16 16:53:24 +0530

    Add functionality to delete blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Post
 
@@ -24,3 +25,9 @@ class BlogUpdateView(UpdateView):
     model = Post
     fields = ['title', 'body']
     template_name = 'post_edit.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
