5643d8413a28cd1c218504915a0d30de1c890426
Babu Thomas <babuthomas138@gmail.com>
2018-02-16 16:23:07 +0530

    Create view for adding posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from .models import Post
 
@@ -7,6 +8,13 @@ class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
+
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
