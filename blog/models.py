bdf8d9183557d4c1fc258a0b569cf969292c369c
Babu Thomas <babuthomas138@gmail.com>
2018-02-16 16:30:02 +0530

    Redirect to blog post url on save

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -8,6 +9,9 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.id])
 
     def __str__(self):
         return self.title
